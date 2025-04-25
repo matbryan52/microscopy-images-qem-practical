2a87909551582f137eb0006d72c9f019ce930ee9
Matthew Bryan <matthewbryan52@gmail.com>
2025-04-25 15:20:08 +0200

    Update example

--- main.py
+++ main.py
@@ -6,7 +6,5 @@ from qem_practical.simulator_ui import simulator_ui
 rootdir = pathlib.Path(qem_practical.__file__).parent.parent.parent
 
 sim_data = np.load(rootdir / "data" / "particles.npz")
-image = sim_data["data"]
-extent = sim_data["extent"]
 simulator = STEMImageSimulator(**sim_data)
 simulator_ui(simulator).show("stem-simulator")
