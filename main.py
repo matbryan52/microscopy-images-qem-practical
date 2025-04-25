165f25860351546bdf9b9970f34902d26b4c48f5
Matthew Bryan <matthewbryan52@gmail.com>
2025-04-25 13:38:29 +0200

    Update main.py for TP

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pathlib
 import numpy as np
+import qem_practical
 from qem_practical.simulator import STEMImageSimulator
 from qem_practical.simulator_ui import simulator_ui
-rootdir = pathlib.Path(__file__).parent
+rootdir = pathlib.Path(qem_practical.__file__).parent.parent.parent
 
 sim_data = np.load(rootdir / "data" / "particles.npz")
 image = sim_data["data"]
 extent = sim_data["extent"]
 simulator = STEMImageSimulator(**sim_data)
-simulator_ui(simulator).servable("stem-simulator")
+simulator_ui(simulator).show("stem-simulator")
