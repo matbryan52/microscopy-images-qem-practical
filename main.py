983d39934e5607b73707dcef84581d43128e5e30
Matthew Bryan <matthewbryan52@gmail.com>
2025-05-18 13:51:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from qem_practical.simulator import STEMImageSimulator
 
-simulator = STEMImageSimulator.default().show()
+simulator = STEMImageSimulator.default(drift_speed="random")
+survey_image = simulator.survey_image(1e-5)
+survey_image.plot()
